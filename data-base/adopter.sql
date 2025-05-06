f31e64796b6ed60eece9449bcb4898ba1e51fbe9
Deivyson Ricardo Silva dos Santos <131066052+DeivRicardoSS@users.noreply.github.com>
2025-05-06 20:01:39 -0300

    updating tables

--- data-base/adopter.sql
+++ data-base/adopter.sql
@@ -9,3 +9,12 @@ CREATE TABLE Adotante (
     PRIMARY KEY (adt_id),
     FOREIGN KEY (user_id) REFERENCES Usuarios(user_id)
 );
+
+CREATE TABLE Adotante (
+    adt_id INT SERIAL,
+    user_id INT,
+    adt_preferencia TEXT,
+    adt_pontuacao INT,
+    PRIMARY KEY (adt_id),
+    FOREIGN KEY (user_id) REFERENCES Usuarios(user_id)
+);
--- data-base/user.sql
+++ data-base/user.sql
@@ -9,3 +9,35 @@ CREATE TABLE Usuarios (
     user_tipo TIPO_USUARIO,
     PRIMARY KEY (user_id)
 );
+
+CREATE TABLE Usuarios (
+    user_id VARCHAR(11) NOT NULL, /*CPF*/
+    user_nome VARCHAR(200),
+    user_telefone VARCHAR(15),
+    user_email VARCHAR(200),
+    user_senha VARCHAR(100),
+    user_token VARCHAR(62)
+    user_dtcriacao TIMESTAMP,
+    PRIMARY KEY (user_id);
+);
+
+CREATE TABLE Endereco (
+    end_id INT SERIAL,
+    user_id INT NOT NULL,
+    end_cep VARCHAR(8),
+    end_estado VARCHAR(200),
+    end_cidade VARCHAR(200),
+    end_bairro VARCHAR(200),
+    end_rua VARCHAR(200),
+    end_numero INT,
+    end_complemento VARCHAR(100),
+    PRIMARY KEY (end_id),
+    FOREIGN KEY (user_id) REFERENCES Usuarios(user_id)
+);
+
+CREATE TABLE Tipo (
+    tipo_id INT SERIAL,
+    tipo_nome VARCHAR(200),
+    tipo_desc TEXT,
+    PRIMARY KEY (tipo_id)
+);
