6a276bd74c6d4f60be476980d2bb5ca94e98357c
Deivyson Ricardo Silva dos Santos <131066052+DeivRicardoSS@users.noreply.github.com>
2025-05-19 20:01:53 -0300

    adicionando mensagens no chat

--- data-base/chatAdotanteOng.sql
+++ data-base/chatAdotanteOng.sql
@@ -1,11 +1,22 @@
 CREATE TABLE ChatAdotanteOng (
     chat_id SERIAL,
     ped_id INT NOT NULL,
-    user_id_remetente VARCHAR(11) NOT NULL,
+    ong_id INT NOT NULL,
+    adt_id INT NOT NULL,
     chat_mensagem TEXT NOT NULL,
-    chat_data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    chat_lida BOOLEAN DEFAULT FALSE,
+    chat_data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (chat_id),
     FOREIGN KEY (ped_id) REFERENCES PedidosPet(ped_id),
-    FOREIGN KEY (user_id_remetente) REFERENCES Usuarios(user_id)
+    FOREIGN KEY (ong_id) REFERENCES Ong(ong_id),
+    FOREIGN KEY (adt_id) REFERENCES Adotante(adt_id)
+);
+
+CREATE TABLE Mensagem (
+    msg_id SERIAL,
+    chat_id INT NOT NULL,
+    msg_conteudo TEXT NOT NULL,
+    msg_data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    msg_visualizado BOOLEAN,
+    PRIMARY KEY (msg_id),
+    FOREIGN KEY (chat_id) REFERENCES ChatAdotanteOng (chat_id)
 );
